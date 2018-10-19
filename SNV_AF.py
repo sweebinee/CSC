3a2d5ff61e0e1152413c43388482069c7dff4f88
sucbin.cho <subin.cho.52@gmail.com>
2018-10-19 17:09:44 +0900

    Update SNV_AF.py

--- SNV_AF.py
+++ SNV_AF.py
@@ -43,18 +43,46 @@ paste PE17_cut.txt PE17_AF.txt > PE17_result.txt
 #
 #
 import os,re
-maindir = '/storage2/Project/CSC/WES/03_SNV/MuTect2'
+maindir = '/storage2/Project/CSC/WES/03_SNV/MuTect'
 
-result = open("%s/%sPE17_result.txt"%(maindir,s),'r') 
-	result_lines = result.readlines()
-	filtered_result = open("%s/%s.txt"%(maindir,s),'w')
-	for i in result_lines:
-		CHR = i.split('\t')[3]
-		POS = i.split('\t')[4]
-		PASS = i.split('\t')[10]
-#		AF = i.split('\t')[13].split(':')[4]
-		if PASS == "PASS":
-			filtered_result.write("%s\t%s\n"%(CHR,POS))
-	result.close()
-	filtered_result.close()
+result = open("%s/PE17_cut.txt"%maindir,'r') 
+result_lines = result.readlines()
+filtered_result = open("%s/1.txt"%maindir,'w')
+for i in result_lines:
+	BC_AF = float(i.split('\t')[8].split(':')[4])
+	PE_AF = float(i.split('\t')[9].split(':')[4])
+	if (BC_AF != 1.0) & (PE_AF == 1.0) :
+		filtered_result.write("%s"%i)
+
+result.close()
+filtered_result.close()
 #
+## PE_AF-BC_AF 하고 histogram 그리는 code in python
+import pandas as pandas
+import matplotlib; matplotlib.use('Agg') # Must be before importing matplotlib.pyplot or pylab!
+import matplotlib.pyplot as plt
+import os, re
+import numpy as np
+import seaborn as sb
+
+sample='PE32'
+AF_hist(sample)
+
+def AF_hist(sample):
+	maindir = '/storage2/Project/CSC/WES/03_SNV/MuTect'
+	result = open("%s/%s_cut.txt"%(maindir,sample),'r') 
+	result_lines = result.readlines()
+	AF_array = np.array([])
+	for i in result_lines:
+		BC_AF = float(i.split('\t')[8].split(':')[4])
+		PE_AF = float(i.split('\t')[9].split(':')[4])
+		dif = abs(float(PE_AF - BC_AF))
+		AF_array = np.append(AF_array,dif)
+	result.close()
+	file_name = '%s_AF_hist.png'%sample
+	save_file = os.path.join(maindir, file_name) 
+	fig = plt.figure() 
+	sb.distplot(AF_array, rug=True)
+	fig.savefig(save_file)
+
+#################################################
