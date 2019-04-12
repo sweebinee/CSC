33817f8ee4dda82a3d5fc97f7d505debf1d8e9a2
sweebinee <cho794562@gmail.com>
2019-04-12 20:18:58 +0900

    d

--- MAGIC.R
+++ MAGIC.R
@@ -6,8 +6,10 @@ library(Rmagic)
 library(ggplot2)
 library(viridis)
 library(phateR)
+library(scFunctions)
 
-impute_seurat_MAGIC(seurat_object)
+blood = readRDS(file="/storage2/Project/CSC/10X/DGIST_data02/BC_Hematopoietic_cell_data/BC_blood_seurat.rds")
+impute_seurat_MAGIC(blood)
 
 data(magic_testdata)
 MAGIC_data <- magic(magic_testdata, genes=c("VIM", "CDH1", "ZEB1"))
